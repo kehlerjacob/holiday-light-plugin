121fd4610942bf0a6754920bef893380908c26cb
Jacob Kehler <jacobkehler@Jacobs-MacBook-Air.local>
2025-11-28 22:07:54 -0500

    Refactor app into embeddable widget

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('holiday-light-widget-root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+} else {
+  console.warn('Holiday Light Widget: Target element "holiday-light-widget-root" not found.');
+}
